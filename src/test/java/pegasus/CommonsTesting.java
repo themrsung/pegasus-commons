541e642415c60528258cadbc2b571dc87abab6aa
themrsung <themrsung@gmail.com>
2023-09-23 19:42:02 +0900

    Stuff

--- src/test/java/pegasus/CommonsTesting.java
+++ src/test/java/pegasus/CommonsTesting.java
@@ -1,33 +1,12 @@
 package pegasus;
 
-import pegasus.graphics.Vertex3;
-import pegasus.tensor.Vector3;
-import pegasus.tuple.Carousel;
-import pegasus.tuple.Tuple;
+import pegasus.util.MassUnit;
 
 public class CommonsTesting {
     public static void main(String[] args) {
-        long n = 10000000000L;
-        long t1, t2;
+        double massKg = 1055;
+        double massLb = MassUnit.convert(MassUnit.KILOGRAM, MassUnit.POUND, massKg);
 
-        t1 = System.currentTimeMillis();
-
-        for (long i = 0; i < n; i++) {
-            Vector3.POSITIVE_X.normalize();
-        }
-
-        t2 = System.currentTimeMillis();
-
-        System.out.println(t2 - t1);
-
-        t1 = System.currentTimeMillis();
-
-        for (long i = 0; i < n; i++) {
-            Vertex3.RIGHT.normalize();
-        }
-
-        t2 = System.currentTimeMillis();
-
-        System.out.println(t2 - t1);
+        System.out.println(massLb);
     }
 }

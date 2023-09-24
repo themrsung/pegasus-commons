2b4001f40f32690645c6ea74ea0ddb26be1ae9f3
themrsung <themrsung@gmail.com>
2023-09-24 11:15:21 +0900

    Stuff

---
+++ src/main/java/pegasus/container/Containers.java
@@ -0,0 +1,74 @@
+package pegasus.container;
+
+import pegasus.exception.IllegalInstanceException;
+
+/**
+ * Contains container utilities.
+ */
+public final class Containers {
+    //
+    // Read-only
+    //
+
+    /**
+     * Creates a read-only reference to the provided container {@code c}'s value.
+     *
+     * @param c   The container of which to reference
+     * @param <T> The type of object to reference
+     * @return A read-only reference to the provided container {@code c}'s value
+     */
+    public static <T> ObjectContainer<T> readOnly(ObjectContainer<? extends T> c) {
+        return new VariableReference<>(c, VariableReference.readOnlySetter());
+    }
+
+    /**
+     * Creates a read-only reference to the provided container {@code c}'s value.
+     *
+     * @param c The container of which to reference
+     * @return A read-only reference to the provided container {@code c}'s value
+     */
+    public static DoubleContainer readOnly(DoubleContainer c) {
+        return new DoubleVariableReference(c, DoubleVariableReference.readOnlySetter());
+    }
+
+    /**
+     * Creates a read-only reference to the provided container {@code c}'s value.
+     *
+     * @param c The container of which to reference
+     * @return A read-only reference to the provided container {@code c}'s value
+     */
+    public static LongContainer readOnly(LongContainer c) {
+        return new LongVariableReference(c, LongVariableReference.readOnlySetter());
+    }
+
+    /**
+     * Creates a read-only reference to the provided container {@code c}'s value.
+     *
+     * @param c The container of which to reference
+     * @return A read-only reference to the provided container {@code c}'s value
+     */
+    public static FloatContainer readOnly(FloatContainer c) {
+        return new FloatVariableReference(c, FloatVariableReference.readOnlySetter());
+    }
+
+    /**
+     * Creates a read-only reference to the provided container {@code c}'s value.
+     *
+     * @param c The container of which to reference
+     * @return A read-only reference to the provided container {@code c}'s value
+     */
+    public static IntContainer readOnly(IntContainer c) {
+        return new IntVariableReference(c, IntVariableReference.readOnlySetter());
+    }
+
+    //
+    // Miscellaneous
+    //
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Containers() {
+        throw new IllegalInstanceException(this);
+    }
+}

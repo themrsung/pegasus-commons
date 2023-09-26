704908cd7b69d99bfe9dc187e29ca6aa28ab5430
themrsung <themrsung@gmail.com>
2023-09-26 09:22:31 +0900

    Stuff

--- src/test/java/pegasus/CommonsTesting.java
+++ src/test/java/pegasus/CommonsTesting.java
@@ -1,16 +1,39 @@
 package pegasus;
 
 import pegasus.container.DoubleContainer;
+import pegasus.graphics.Viewport;
 import pegasus.pointer.DoublePointer;
+import pegasus.scheduler.AtomicScheduler;
+import pegasus.scheduler.Scheduler;
 import pegasus.tensor.Tensors;
 import pegasus.tensor.Vector2;
 
+import javax.swing.*;
+
 public class CommonsTesting {
+    static final JFrame frame;
+    static final JPanel panel;
+    static final Viewport viewport;
+
+    static {
+        frame = new JFrame();
+        panel = new JPanel();
+        viewport = new Viewport();
+
+        frame.add(panel);
+        panel.add(viewport);
+
+        frame.setSize(1920, 1080);
+    }
+
+    static Scheduler scheduler = new AtomicScheduler();
+
     public static void main(String[] args) {
-        Vector2 v = new Vector2(0, 100);
-        Vector2 p = v.rotate(Math.PI / 2);
+        scheduler.registerRepeatingTask((t, i) -> frame.repaint(), 100);
 
-        System.out.println(p);
 
+
+        frame.setVisible(true);
+        scheduler.start();
     }
 }
